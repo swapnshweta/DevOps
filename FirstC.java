1000a87cf6ccadb1bd99b3f82132d83bfb541661
swapnshweta <pawar.swapnil59@gmail.com>
2025-02-15 12:35:36 +0530

    first

--- FirstC.java
+++ FirstC.java
@@ -2,6 +2,6 @@ class FirstC {
     public static void main(String[] args) {
         System.out.println("Hello worl");
         System.out.println("Shivay");
-        System.out.println("Welcome to Jenkins");
+        
     }
 }
